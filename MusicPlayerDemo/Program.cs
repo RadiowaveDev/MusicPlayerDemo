7b2d7f8f547a096c89a6f3977bce7af2dff7ad4b
Angel Gonzales <asolaris4536@gmail.com>
2025-04-11 22:35:45 -0500

    Agregar archivos de proyecto.

---
+++ MusicPlayerDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicPlayerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song1 = new Song("Bohemian Rapsody","Another one bites the dust","Queen song","Rock");
+            song1.Album="Fat bottom girl";
+            Console.WriteLine(song1.Album);
+
+        }
+    }
+}
